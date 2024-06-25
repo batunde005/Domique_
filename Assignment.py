f95b32a8faf4ed0e45e49a96caf4b3e8fd71ff9a
Batunde Abioro <batunde.abioro@gmail.com>
2024-06-25 12:20:53 +0100

    Assignment Updated
    Additional efforts were deployed this morning to attempt 2 of of 4 assignment

--- Assignment.py
+++ Assignment.py
@@ -1,1 +1,15 @@
-Additional update on my assignment done this morning 
+r=1200
+g=300
+t=150
+e=100
+m=50
+total=r+g+t+e+m
+print(total)
+daily= total/30
+print(daily)
+
+
+numbers=2+4+6+8+10
+print(numbers)
+mean=numbers/5
+print(mean)
