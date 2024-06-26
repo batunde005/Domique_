b00a95b3652865f15cba1b47ae0885f1afd1a603
Batunde Abioro <batunde.abioro@gmail.com>
2024-06-26 15:15:12 +0100

    more sample test on second class

--- testtime.py
+++ testtime.py
@@ -1,36 +1,7 @@
-name = "tunde"
-last_letter = name [0]
-print(last_letter)
-
-cih2_student = "Kolawole"
-last_letter = cih2_student[7]
-print(last_letter)
-
-name = "Abacha"
-first_letter = name[0]
-print(first_letter)
-
-sentence = "kehinde is teaching python programming"
-a_letter_from_a_sentence = sentence [21]
-print(a_letter_from_a_sentence)
 
 
+name= input("enter name ")
+age= input(" age ")
+hubby= input(" hubby ")
 
-my_name = "Tunde"
-a_positive_index = my_name [0]
-print(a_positive_index)
-a_positive_index = my_name [-5]
-print(a_positive_index)
-
-
-
-my_hubby = "i love codeng" #we are to prit love
-the_word_love = my_hubby[:5]
-#the_word_love = my_hubby[::-1]
-print(the_word_love)
-
-a_name = "kehinde"
-print(a_name[:4])
-
-
-super_eagle = "finidi george"
+print(" hello " + name + " you are " + age + " years old and your hubby is " + hubby)
