bc2489b795c9da779cbaa607ffd2ea98125c9310
Batunde Abioro <batunde.abioro@gmail.com>
2024-07-01 10:20:00 +0100

    cost v selling price assignment

--- assignment2.py
+++ assignment2.py
@@ -1,26 +1,12 @@
-first_sentence = "I thnk i would love to be like Elon"
-second_sentence = "not the elon at mushin, the main one, Musk"
-print("the founder of" + first_sentence[-4:] + second_sentence[-4:])
 
-
-
-name = "tayo"
-age = 10
-print(f"my name is {name} and i am {10} years old")
-
-
-# accept a float value from a user,convert to string te convert back to intejer
-
-user_input = float(input('how many dog'))
-user_str = (6)
-user_int = int(user_input)
-
-print(user_str)
-print(user_int)
-
-
-
-
-
+Supermarket_Sales = int(input("Enter Product Details"))
+Name = int(input("Product Name"))
+Cost_Price = int(input("Cost Price"))
+SellinG_Price = int(input("Selling Price"))
+Rice = (200)
+Yam = (300)
+Dodo = (400)
+Piza = (500)
+Ans = Supermarket_Sales - Bread
 
 
