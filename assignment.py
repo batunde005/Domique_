171de88204146deae66c16c5a9ecc21bd6e29c82
Batunde Abioro <batunde.abioro@gmail.com>
2024-07-01 09:36:47 +0100

    Friday Assignment

---
+++ assignment.py
@@ -0,0 +1,31 @@
+#Student_Name = (Tolade)
+User = int(input("Enter The Student Total Marks"))
+Maths = (20)
+Eng = (40)
+Yor = (60)
+Phy = (80)
+Art = (100)
+Gns = (10)
+Agr = (5)
+Bio = (15)
+Com = (25)
+Che = (30)
+Total_Score = ("385")
+if user == Total_Score:
+    print("70")
+elif user <= Total_Score:
+    print("60-69")
+elif user <=Total_Score:
+    print("50-59")
+elif user <= Total_Score:
+    print("40-49")
+elif user <= Total_Score:
+    print("30-39")
+else:
+    print("<30")
+
+
+
+
+
+
